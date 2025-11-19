db354cbccb802086ccacda0d731ecfc23b846f5a
Vikvins <116019135+Vikvins@users.noreply.github.com>
2025-11-19 22:39:56 +0300

    files uploaded

---
+++ Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace passwords_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnValidate = new System.Windows.Forms.Button();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.cbUpper = new System.Windows.Forms.CheckBox();
+            this.cbDigits = new System.Windows.Forms.CheckBox();
+            this.cbSymbols = new System.Windows.Forms.CheckBox();
+            this.numericLength = new System.Windows.Forms.NumericUpDown();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbRussian = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericLength)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(293, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Генератор и валидатор паролей";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(429, 134);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(163, 61);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "Сгенерировать пароль";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnValidate
+            // 
+            this.btnValidate.Location = new System.Drawing.Point(429, 270);
+            this.btnValidate.Name = "btnValidate";
+            this.btnValidate.Size = new System.Drawing.Size(332, 61);
+            this.btnValidate.TabIndex = 3;
+            this.btnValidate.Text = "Проверить пароль";
+            this.btnValidate.UseVisualStyleBackColor = true;
+            this.btnValidate.Click += new System.EventHandler(this.btnValidate_Click);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(426, 198);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(0, 16);
+            this.lblPassword.TabIndex = 4;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(426, 334);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 16);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Click += new System.EventHandler(this.lblResult_Click);
+            // 
+            // cbUpper
+            // 
+            this.cbUpper.AutoSize = true;
+            this.cbUpper.Location = new System.Drawing.Point(31, 134);
+            this.cbUpper.Name = "cbUpper";
+            this.cbUpper.Size = new System.Drawing.Size(240, 20);
+            this.cbUpper.TabIndex = 6;
+            this.cbUpper.Text = "Использовать заглавные буквы";
+            this.cbUpper.UseVisualStyleBackColor = true;
+            // 
+            // cbDigits
+            // 
+            this.cbDigits.AutoSize = true;
+            this.cbDigits.Location = new System.Drawing.Point(31, 160);
+            this.cbDigits.Name = "cbDigits";
+            this.cbDigits.Size = new System.Drawing.Size(170, 20);
+            this.cbDigits.TabIndex = 7;
+            this.cbDigits.Text = "Использовать цифры";
+            this.cbDigits.UseVisualStyleBackColor = true;
+            // 
+            // cbSymbols
+            // 
+            this.cbSymbols.AutoSize = true;
+            this.cbSymbols.Location = new System.Drawing.Point(31, 186);
+            this.cbSymbols.Name = "cbSymbols";
+            this.cbSymbols.Size = new System.Drawing.Size(214, 20);
+            this.cbSymbols.TabIndex = 8;
+            this.cbSymbols.Text = "Использовать спецсимволы";
+            this.cbSymbols.UseVisualStyleBackColor = true;
+            // 
+            // numericLength
+            // 
+            this.numericLength.Location = new System.Drawing.Point(220, 290);
+            this.numericLength.Name = "numericLength";
+            this.numericLength.Size = new System.Drawing.Size(120, 22);
+            this.numericLength.TabIndex = 9;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(598, 134);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(163, 61);
+            this.btnCopy.TabIndex = 10;
+            this.btnCopy.Text = "Скопировать в буфер обмена";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(388, 72);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(204, 22);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(228, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Введите ваш пароль:";
+            // 
+            // cbRussian
+            // 
+            this.cbRussian.AutoSize = true;
+            this.cbRussian.Location = new System.Drawing.Point(31, 212);
+            this.cbRussian.Name = "cbRussian";
+            this.cbRussian.Size = new System.Drawing.Size(191, 20);
+            this.cbRussian.TabIndex = 12;
+            this.cbRussian.Text = "Добавить русские буквы";
+            this.cbRussian.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 292);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(177, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Изменить размер пароля:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbRussian);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.numericLength);
+            this.Controls.Add(this.cbSymbols);
+            this.Controls.Add(this.cbDigits);
+            this.Controls.Add(this.cbUpper);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.btnValidate);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericLength)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnValidate;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.CheckBox cbUpper;
+        private System.Windows.Forms.CheckBox cbDigits;
+        private System.Windows.Forms.CheckBox cbSymbols;
+        private System.Windows.Forms.NumericUpDown numericLength;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox cbRussian;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace passwords_project
+{
+    public partial class Form1 : Form
+    {
+        private Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            int length = (int)numericLength.Value;
+            bool useUpper = cbUpper.Checked;
+            bool useDigits = cbDigits.Checked;
+            bool useSymbols = cbSymbols.Checked;
+            bool useRussian = cbRussian.Checked;
+
+            string password = GeneratePassword(length, useUpper, useDigits, useSymbols, useRussian);
+            lblPassword.Text = password;
+        }
+
+        private string GeneratePassword(int length, bool useUpper, bool useDigits, bool useSymbols, bool useRussian)
+        {
+            if (length < 1) return "";
+
+            string lower = "abcdefghijklmnopqrstuvwxyz";
+            string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string digits = "0123456789";
+            string symbols = "!@#$%^&*()-_=+";
+            string russian = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
+            string allChars = lower;
+
+            if (useUpper) allChars += upper;
+            if (useDigits) allChars += digits;
+            if (useSymbols) allChars += symbols;
+            if (useRussian) allChars += russian;
+
+            List<char> passwordChars = new List<char>();
+
+            passwordChars.Add(lower[rnd.Next(lower.Length)]);
+            if (useUpper) passwordChars.Add(upper[rnd.Next(upper.Length)]);
+            if (useDigits) passwordChars.Add(digits[rnd.Next(digits.Length)]);
+            if (useSymbols) passwordChars.Add(symbols[rnd.Next(symbols.Length)]);
+            if (useRussian) passwordChars.Add(russian[rnd.Next(russian.Length)]);
+
+            for (int i = passwordChars.Count; i < length; i++)
+            {
+                passwordChars.Add(allChars[rnd.Next(allChars.Length)]);
+            }
+
+            return new string(passwordChars.OrderBy(x => rnd.Next()).ToArray());
+        }
+
+
+
+
+        private void btnValidate_Click(object sender, EventArgs e)
+        {
+            string password = txtPassword.Text;
+            int score = 0;
+
+            if (password.Length >= 12) score += 2;
+            else if (password.Length >= 8) score += 1;
+
+            if (password.Any(char.IsLower)) score += 1;
+
+            if (password.Any(char.IsUpper)) score += 1;
+
+            if (password.Any(char.IsDigit)) score += 1;
+
+            if (password.Any(ch => "!@#$%^&*()-_=+".Contains(ch))) score += 1;
+
+            if (!HasRepeatedChars(password)) score += 1;
+
+            if (!HasSequentialChars(password)) score += 1;
+
+            if (password.Length >= 16) score += 1;
+
+            if (password.Any(char.IsLower) && password.Any(char.IsUpper) &&
+                password.Any(char.IsDigit) && password.Any(ch => "!@#$%^&*()-_=+".Contains(ch)))
+                score += 1;
+
+            if (password.Distinct().Count() >= 8) score += 1;
+
+            lblResult.Text = $"Надёжность: {score}/10";
+
+            if (score <= 3) lblResult.ForeColor = Color.Red;
+            else if (score <= 6) lblResult.ForeColor = Color.Orange;
+            else if (score <= 8) lblResult.ForeColor = Color.Blue;
+            else lblResult.ForeColor = Color.Green;
+        }
+
+    
+        private bool HasRepeatedChars(string password)
+        {
+            for (int i = 1; i < password.Length; i++)
+            {
+                if (password[i] == password[i - 1]) return true;
+            }
+            return false;
+        }
+
+        private bool HasSequentialChars(string password)
+        {
+            for (int i = 0; i < password.Length - 2; i++)
+            {
+                int diff1 = password[i + 1] - password[i];
+                int diff2 = password[i + 2] - password[i + 1];
+                if (diff1 == 1 && diff2 == 1) return true;
+            }
+            return false;
+        }
+
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(lblPassword.Text))
+            {
+                Clipboard.SetText(lblPassword.Text);
+                MessageBox.Show("Пароль скопирован в буфер обмена!");
+            }
+            else
+            {
+                MessageBox.Show("Сначала сгенерируйте пароль!");
+            }
+        }
+
+        private void lblResult_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
